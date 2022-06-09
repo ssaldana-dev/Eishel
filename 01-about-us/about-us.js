c0f2a436a4ad6dc480fd32e920fea819c91f6400
sergio saldana <ssaldana.rangel@gmail.com>
2022-06-09 16:26:53 -0500

    upgraded about-us section and added JS

---
+++ 01-about-us/about-us.js
@@ -0,0 +1,16 @@
+function ShowValue () {
+    let valor = document.createElement('article');
+    valor.className = 'main-identity-values-value';
+    let imagen_valor = document.createElement('img');
+    imagen_valor.src = './assets/icons/teamwork-icon.svg'
+    imagen_valor.className = 'main-identity-values-value-img';
+    let titulo_valor = document.createElement('h3');
+    titulo_valor.className = 'main-identity-values-value-title';
+    let texto_titulo_valor = document.createTextNode('Trabajo en equipo');
+    let parrafo_valor = document.createElement('p');
+    parrafo_valor.className = 'main-identity-values-value-text';
+    let texto_parrafo_valor = 'Somos un conjunto de personas comprometidas con nuestro rol para lograr un objetivo en común. (Personas, organización y objetivo común).'
+
+    
+}
+
