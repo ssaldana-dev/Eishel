1b9c9d3f1bd4b3ca9fd1e6935ed47abbcff98988
sergio saldana <ssaldana.rangel@gmail.com>
2022-07-07 16:25:51 -0500

    changes some assets and added media quertys

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const donationsContainer = document.getElementById('donations-container');
-const donationsContainerImg = document.getElementById('donations-container-image');
+const donationsContainerImg = document.getElementById('donations-image');
 const bbvaButton = document.getElementById('bbva-button');
 bbvaButton.addEventListener('click', donationsChange);
 
 const bbvaInformationElement = document.createElement('article');
 bbvaInformationElement.className = 'donations-flyer-container-information';
 const bbvaInformationElementParagraphOne = document.createElement('p');
+bbvaInformationElementParagraphOne.className = 'donations-flyer-container-information-paragraph';
 const bbvaInformationElementParagraphOneText = document.createTextNode('Eishel Nuestro Hogar, A.C. ')
 const bbvaInformationElementParagraphTwo = document.createElement('p');
+bbvaInformationElementParagraphTwo.className = 'donations-flyer-container-information-paragraph';
 const bbvaInformationElementParagraphTwoText = document.createTextNode('BBVA')
 const bbvaInformationElementParagraphTree = document.createElement('p');
+bbvaInformationElementParagraphTree.className = 'donations-flyer-container-information-paragraph';
 const bbvaInformationElementParagraphTreeText = document.createTextNode('Clabe 012540004513728909')
 const bbvaInformationElementParagraphFour = document.createElement('p');
+bbvaInformationElementParagraphFour.classList = 'donations-flyer-container-information-paragraph';
 const bbvaInformationElementParagraphFourText =document.createTextNode('Cuenta 0451372890')
 
 bbvaInformationElement.appendChild(bbvaInformationElementParagraphOne);
@@ -32,6 +36,7 @@ function donationsChange () {
         donationsContainer.replaceChild(bbvaInformationElement, donationsContainer.firstChild);
         donationsContainer.replaceChild(donationsContainerImg, donationsContainer.firstChild);
         donationsContainer.replaceChild(bbvaInformationElement, donationsContainer.firstChild);
+        first = 1;
     }
     if (change == 0) {
         donationsContainer.replaceChild(bbvaInformationElement, donationsContainer.firstChild);
