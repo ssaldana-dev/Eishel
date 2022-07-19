f39e19417b5a833e8c4c8305c6e9a45e5082d052
sergio saldana <ssaldana.rangel@gmail.com>
2022-07-19 12:33:16 -0500

    added the send ticket section

--- 06-collaborators/collaborators.js
+++ 06-collaborators/collaborators.js
@@ -2,3 +2,20 @@ const mainCollaborators = document.getElementById('main-collab');
 const initialHeight = window.innerHeight - 150;
 
 mainCollaborators.style.height = `${initialHeight}px`;
+
+const user = document.getElementById('user-input');
+const password = document.getElementById('password-input');
+const send = document.getElementById('access-button');
+send.addEventListener('click', verifyUser);
+
+const correctUser = 'Eishel';
+const correctPassword = 'User2022';
+
+function verifyUser () {
+    if (user.value == correctUser && password.value == correctPassword) {
+        window.location.href = "../07-support/support.html";
+    }
+    else {
+        alert('Usuario o contraseña inválidos');
+    }
+}
