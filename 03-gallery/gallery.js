f96d922a2e09dfa14832826b4fce9dbb627d7711
sergio saldana <ssaldana.rangel@gmail.com>
2022-07-20 16:12:40 -0500

    upgraded gallery JavaScript and added media queries

--- 03-gallery/gallery.js
+++ 03-gallery/gallery.js
@@ -2,6 +2,7 @@ const title = document.getElementById('title');
 const galleryTitleInstalations = document.getElementById('gallery-title-instalations');
 const galleryTitleEvents = document.getElementById('gallery-title-events');
 const galleryTitleActivities = document.getElementById('gallery-title-activities');
+const galleryBreakPoint = 1280;
 
 title.addEventListener('click', resetCarouselItemsSize);
 galleryTitleInstalations.addEventListener('click', resetCarouselItemsSize,);
@@ -11,18 +12,15 @@ galleryTitleActivities.addEventListener('click', resetCarouselItemsSize,);
 let whatPhotoIsClicked = 0;
 const miniatures = [];
 /*  Cicle que guarda las miniaturas del HTML en un array.   */
-for (let h = 1; h <= 13; h++) {
+for (let h = 1; h <= 12; h++) {
     let idInstalations = 'instalations-' + h;
     let idEvents = 'activities-' + (h);
     let idActivities = 'events-' + (h);
+    h1 = h + 12;
+    h2 = h + 12 + 12;
     miniatures[h] = document.getElementById(idInstalations);
-    if (h >= 13) {
-    } else {
-        h1 = h + 13;
-        h2 = h + 13 + 12;
-        miniatures[h1] = document.getElementById(idActivities);
-        miniatures[h2] = document.getElementById(idEvents);
-    }
+    miniatures[h1] = document.getElementById(idActivities);
+    miniatures[h2] = document.getElementById(idEvents);
 }
 /*  Este ciclo le agrega un eventListener a cada alemento, y agrega una función que guarda el número 
     de la miniatura en el click.    */
@@ -33,6 +31,12 @@ for (let k = 1;k < miniatures.length; k++) {
 const secondClick = [];
 for (let l = 1; l < miniatures.length; l++) {
     secondClick[l] = false;
+}
+if (window.innerWidth >= galleryBreakPoint) {
+    for (let m = 1; m < secondClick.length; m++) {
+        secondClick[m] = true;
+        console.log (`secondclick[${m}] = true`)
+    }
 }
 /*  Esta función hace grande la miniatura al primer click y al segundo consecutivo abre el banner.  */
 function makeItBigger () {
@@ -52,17 +56,23 @@ function resetCarouselItemsSize () {
         miniatures[i].className = 'gallery-carousel-photo';
     }
     miniatures[1].className = 'gallery-carousel-photo carousel-first-photo';
-    miniatures[13].className = 'gallery-carousel-photo carousel-last-photo';
-    miniatures[14].className = 'gallery-carousel-photo carousel-first-photo';
-    miniatures[25].className = 'gallery-carousel-photo carousel-last-photo';
-    miniatures[26].className = 'gallery-carousel-photo carousel-first-photo';
-    miniatures[37].className = 'gallery-carousel-photo carousel-last-photo';
+    miniatures[12].className = 'gallery-carousel-photo carousel-last-photo';
+    miniatures[13].className = 'gallery-carousel-photo carousel-first-photo';
+    miniatures[24].className = 'gallery-carousel-photo carousel-last-photo';
+    miniatures[25].className = 'gallery-carousel-photo carousel-first-photo';
+    miniatures[36].className = 'gallery-carousel-photo carousel-last-photo';
     resetClickValues()
 }
 /*  La función resetClickValues, resetea los valores del click. */
 function resetClickValues () {
-    for (let j = 0; j < secondClick.length; j++) {
-        secondClick[j] = false;
+    if (window.innerWidth >= galleryBreakPoint) {
+        for (let n = 0; n < secondClick.length; n++) {
+            secondClick[n] = true;
+        }
+    } else {
+        for (let j = 0; j < secondClick.length; j++) {
+            secondClick[j] = false;
+        }
     }
 }
 /*  Las siguiente parte contiene el dinamismo del banner, falta comentar su funcionamiento. */
@@ -72,35 +82,31 @@ const bannerImageBack = document.getElementById('display-photo-photoBack')
 let currentBannerPosition = 0;
 /*  Este ciclo crea y guarda las rutas de las imágnes del banner en un array.   */
 const bannerImageSources = []
-for (let l = 1; l <= 13; l++) {
+for (let l = 1; l <= 12; l++) {
     let instalationsPath = `./assets/imgs/mobile-banner/instalaciones-${l}.jpg`;
     let activitiesPath = `./assets/imgs/mobile-banner/actividades-${l}.jpg`;
     let eventsPath = `./assets/imgs/mobile-banner/eventos-${l}.jpg`;
     bannerImageSources[l] = instalationsPath;
-    if (l >= 13) {
-
-    } else {
-        bannerImageSources[l + 13] = eventsPath;
-        bannerImageSources[l + 12 + 13] = activitiesPath;
-    }
+    bannerImageSources[l + 12] = eventsPath;
+    bannerImageSources[l + 12 + 12] = activitiesPath;
 }
-
+/*  Esta función cierra el baner y reinicia los tamaños del carousel.   */ 
 function closeBanner () {
     bannerImageBack.className = 'gallery-display-container-photo-photoBack';
     bannerImage.className = 'gallery-display-container-photo';
     banner.className = 'gallery-display off';
     resetCarouselItemsSize();
 }
-
+/*  Esta función abre el banner y lo posiciona donde se haya dado click.    */
 function openBanner () {
     bannerImage.src = bannerImageSources[currentBannerPosition]
     banner.className = 'gallery-display on';
 }
-
+/*  Esta función cambia la imagen del baner a la anterior y despliega una animación.    */
 function slideImageBack() {
     if (currentBannerPosition <= 1) {
-    } else if (currentBannerPosition == 14) {
-    } else if (currentBannerPosition == 26) {
+    } else if (currentBannerPosition == 13) {
+    } else if (currentBannerPosition == 25) {
     } else {
         bannerImage.src = bannerImageSources[currentBannerPosition - 1];
         bannerImageBack.src = bannerImageSources[currentBannerPosition];
@@ -109,11 +115,11 @@ function slideImageBack() {
         fadeOut();
     }
 }
-
+/*  Esta función cambia la imagen del baner a la posterior y despliega una animación.   */
 function slideImageFront () {
-    if (currentBannerPosition == 13) {
-    } else if (currentBannerPosition == 25) {
-    } else if (currentBannerPosition == 37) {
+    if (currentBannerPosition == 12) {
+    } else if (currentBannerPosition == 24) {
+    } else if (currentBannerPosition == 36) {
     } else {
         bannerImage.src = bannerImageSources[currentBannerPosition + 1]
         bannerImageBack.src = bannerImageSources[currentBannerPosition];
